9b1be25241ff17557b6d2c05ae428ccb002646a9
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2019-10-06 14:22:28 +0200

    calendar event

--- app/controllers/member/pages_controller.rb
+++ app/controllers/member/pages_controller.rb
@@ -3,6 +3,7 @@ module Member
   class PagesController < MemberController
 
     def dashboard
+      @calendar_events = CalendarEvent.where(:date.gte => DateTime.now, :date.lte => DateTime.now + 2.weeks).asc(:date)
     end
 
 
