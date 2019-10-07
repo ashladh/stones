f29829ed083f08f1267094878488ac0f0aa43bd2
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2019-10-07 14:28:29 +0200

    event_participation

--- app/controllers/member/event_participations_controller.rb
+++ app/controllers/member/event_participations_controller.rb
@@ -3,10 +3,12 @@ module Member
   class EventParticipationsController < MemberController
 
     before_action :get_event_participation, only: [:update]
-    before_action :check_character_ownership, only: [:create, :update]
+    before_action :check_character_ownership, only: [:update]
 
     def create
       @event_participation = EventParticipation.new(event_participation_params)
+      check_character_ownership
+
       @event_participation.save!
       redirect_to member_calendar_event_path(@event_participation.calendar_event.id)
     end
@@ -36,7 +38,7 @@ module Member
 
     def check_character_ownership
       character = @event_participation.character
-      render_401 if character && character.user != current_user
+      render_401 unless current_user.officer? || (character && character.user == current_user)
     end
 
     
